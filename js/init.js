689e19064364aae11e87885f5b72cabbc5b21ba3
anpacove <anpacove@hotmail.com>
2017-10-29 19:11:14 -0500

    2 Version

--- js/init.js
+++ js/init.js
@@ -1,8 +1,19 @@
 (function($){
   $(function(){
 
-    $('.button-collapse').sideNav();
     $('.parallax').parallax();
+
+    // Pushpin Demo Init
+    if ($('.pushpin-demo-nav').length) {
+      $('.pushpin-demo-nav').each(function() {
+        var $this = $(this);
+        var $target = $('#' + $(this).attr('data-target'));
+        $this.pushpin({
+          top: $target.offset().top,
+          bottom: $target.offset().top + $target.outerHeight() - $this.height()
+        });
+      });
+    }
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
