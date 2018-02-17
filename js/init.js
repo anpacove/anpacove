b100e38a9f881f0ef03b02af9fb17a24f0e414a7
Zero <bermudez.edison@gmail.com>
2018-02-17 18:42:47 -0500

    Minor adjustments

--- js/init.js
+++ js/init.js
@@ -1,13 +1,7 @@
 (function ($) {
   $(function () {
 
-    $('#orientancion').css('height', parseFloat($('#apoyo').css('height')));
-    $('#relajacion').css('height', parseFloat($('#eficacia').css('height')));
-    $('#psicoterapeuta').css('height', parseFloat($('#coach').css('height')));
-    $('#PerfilEstudios').css('height', parseFloat($('#PerfilDescripcion').css('height')));
-    $('#PerfilFinal').css('height', parseFloat($('#PerfilProfesional').css('height')));
-    $('.parallax').parallax();
-    $('.slider').slider();
+
 
 
     var options = [
@@ -34,6 +28,14 @@
       },
     ];
     Materialize.scrollFire(options);
+
+    $('#orientancion').css('height', parseFloat($('#apoyo').css('height')));
+    $('#relajacion').css('height', parseFloat($('#eficacia').css('height')));
+    $('#psicoterapeuta').css('height', parseFloat($('#coach').css('height')));
+    $('#PerfilEstudios').css('height', parseFloat($('#PerfilDescripcion').css('height')));
+    $('#PerfilFinal').css('height', parseFloat($('#PerfilProfesional').css('height')));
+    $('.parallax').parallax();
+    $('.slider').slider();
 
   });
 
